16190316d36dd200d70529a1960a5a826e6212c2
MichaelPhuc <49359137+MichaelPhuc@users.noreply.github.com>
2022-11-04 17:46:07 -0700

    small destination page changes

--- src/components/destination/Destination.jsx
+++ src/components/destination/Destination.jsx
@@ -5,12 +5,16 @@ export default function Destination() {
     return (
         <div className="destination">
             <div className="left">
+                <div>
                 <h5><span>01</span> PICK YOUR DESTINATION</h5>
+                </div>
+                
                     
                 <img src="assets/destination/image-moon.png" alt="The Moon" className="moon"/>
             </div>
             <div className="right">
-                <p className="destination-text">
+                
+                 <div>
                     
 
                 Moon
@@ -19,18 +23,18 @@ export default function Destination() {
                 Titan
 
                 Moon
-
-                See our planet as you’ve never seen it before. A perfect relaxing trip away to help 
-                regain perspective and come back refreshed. While you’re there, take in some history 
-                by visiting the Luna 2 and Apollo 11 landing sites.
-
+                <p className="destination-text">
+                    See our planet as you’ve never seen it before. A perfect relaxing trip away to help 
+                    regain perspective and come back refreshed. While you’re there, take in some history 
+                    by visiting the Luna 2 and Apollo 11 landing sites.
+                </p>
                 Avg. distance
                 384,400 km
 
                 Est. travel time
                 3 days
-                </p>
-            
+             
+                </div>   
             </div>
         </div>
 
