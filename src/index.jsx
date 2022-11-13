efb52a86c580852456ccf82d06da3dffd8d3a160
MichaelPhuc <49359137+MichaelPhuc@users.noreply.github.com>
2022-11-13 11:22:31 -0800

    cleaned up code and updated readme

--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import './index.css';
 import App from './App';
-import Home from './components/home/Home';
-import Destination from './components/destination/Destination';
-
-
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
